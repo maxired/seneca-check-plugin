fc40bca5cb0cf613de7ea9d5a7fc84494f138383
Maxired (Maxence Dalmais) <github@maxired.fr>
2015-10-19 10:56:43 +0200

    fix amd improve tests

--- test/check.test.js
+++ test/check.test.js
@@ -7,16 +7,37 @@ var describe = lab.describe
 var it = lab.it
 
 var Check = require('..')
-
+var check
 var assert = require('assert')
 var _ = require('lodash')
 
 describe('Check', function () {
-  it('has 2 methodes', function (done) {
-    assert.ok(Check.checkRepo)
-    assert.ok(_.isFunction(Check.checkRepo))
-    assert.ok(Check.checkFolder)
-    assert.ok(_.isFunction(Check.checkFolder))
+  it('is a Function', function (done) {
+    assert.ok(Check)
+    assert.ok(_.isFunction(Check))
+    done()
+  })
+
+  it('can be initated synchronously', function (done) {
+    check = Check()
+    done()
+  })
+
+  it('check has 2 methodes', function (done) {
+    assert.ok(check.checkRepo)
+    assert.ok(_.isFunction(check.checkRepo))
+    assert.ok(check.checkFolder)
+    assert.ok(_.isFunction(check.checkFolder))
+    done()
+  })
+
+  it('can be initated with params synchronously', function (done) {
+    check = Check({})
+    done()
+  })
+
+  it('can be initated with params synchronously', function (done) {
+    check = Check({fix: true})
     done()
   })
 })
